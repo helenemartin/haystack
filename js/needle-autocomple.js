dabc1b6f5d587fe8997181564988e850376043a8
helenemartin <helenemartin13@hotmail.com>
2017-12-13 23:39:26 +0000

    add images to asset file, create needle-autocomplete.js file, extract image from dummy data on index page

---
+++ js/needle-autocomple.js
@@ -0,0 +1,21 @@
+  var needles = [
+    { value: 'needle', image: "assets/black.png" },
+    { value: 'knitting needle', image: "assets/cream.png"},
+    { value: 'pin', image: "assets/ming.png" },
+    { value: 'obelisk', image: "assets/orange.png"},
+    { value: 'safety pin', image: "assets/purple.png" },
+    { value: 'crochet hook', image: "assets/red.png" },
+    { value: 'bacteria', image: "assets/black.png" },
+    { value: 'Cleopatras needle', image: "assets/ming.png" },
+  ];
+  
+ $('#autocomplete').autocomplete({
+    lookup: needles,
+    onSelect: function(suggestion) {
+      var searchimage = 'a ' + suggestion.value  + ' </br> <img src="' + suggestion.image + '" <p>in a hay stack</p>';
+      // var searchimage = '<p>a </p>'<p> + suggestion.value + "<\p>" </br> <img src="' + suggestion.image + '"<p> in a haystack</p>";
+      $('#outputcontent').html(searchimage);
+    
+    }
+  });  
+});
